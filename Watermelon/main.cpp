41431d4058fcde2ffb4e4c25d23d9677e5e138d0
Tonouewa-kefil <tonouewakefil@gmail.com>
2020-05-25 07:02:55 -0600

    excutable

--- Watermelon/main.cpp
+++ Watermelon/main.cpp
@@ -19,26 +19,26 @@ int main()
         //check if it is two because two is the only even number that does not satify the conditions (No)
         if(W_Weight == 2)
         {
-            printf("No");
+            printf("No \n");
             fflush(stdout);
         }
         //If the weight is even and not two then (Yes)
         else
         {
-            printf("Yes");
+            printf("Yes \n");
             fflush(stdout);
         }
     }
     //Check if the weight is 1 or a prime number or divisible by three of by 9 (No)
     else if (W_Weight == 1 || isPrime(W_Weight) || (W_Weight % 3) == 0 || (W_Weight % 9) == 0)
     {
-        printf("No");
+        printf("No \n");
         fflush(stdout);
     }
     //Every other weight does not satisfy the sitution (No)
     else
     {
-        printf("No");
+        printf("No \n");
         fflush(stdout);
         
     }
